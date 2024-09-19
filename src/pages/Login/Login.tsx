ec593315da8b3d938df0f8157b2c330746cace48
macolmenerori <miguelangelcolmenero@gmail.com>
2024-09-19 17:05:33 +0200

    Pull request template

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -60,26 +60,3 @@ function Login() {
 }
 
 export default Login;
-
-/*
-
-<h2>Login</h2>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Email</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-
-*/
